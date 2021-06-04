9e10f697aac7a66452d357003cad52846684a239
Jonas Söderberg <imevul@gmail.com>
2021-06-04 19:17:19 +0200

    * Fixed loading issue

--- imevul/ui/init.lua
+++ imevul/ui/init.lua
@@ -12,15 +12,15 @@ ui.lib.class = dofile(ui.config.path .. '/lib/class.lua')
 ui.lib.cobalt = dofile(ui.config.cobaltPath .. '/init.lua')
 
 local loadModule = function(module)
-	ui.modules[module] = loadfile(ui.config.path .. '/modules/' .. module .. '.lua')(ui)
+	ui.modules[module] = loadfile(ui.config.path .. '/modules/' .. string.lower(module) .. '.lua')(ui)
 	return ui.modules[module]
 end
 
-UI_Object = loadModule('object')
-UI_Container = loadModule('container')
-UI_Window = loadModule('window')
-UI_Text = loadModule('text')
-UI_Button = loadModule('button')
-UI_App = loadModule('app')
+UI_Object = loadModule('Object')
+UI_Container = loadModule('Container')
+UI_Window = loadModule('Window')
+UI_Text = loadModule('Text')
+UI_Button = loadModule('Button')
+UI_App = loadModule('App')
 
 return ui
